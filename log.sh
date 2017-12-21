816a5206186ec31764ec2cae4e771f42cb5ab33e
Xing Tao Shi <xshi@stuy.edu>
2017-12-21 10:28:30 -0500

    fixed log.sh

--- log.sh
+++ log.sh
@@ -6,10 +6,10 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 ENTRY="$1 -- $DATE\n$2\n"
 
 if [ $# -eq 2 ]; then
-    if [ ! -e devlog.txt ]; then
-        touch devlog.txt
+    if [ ! -e DEVLOG.txt ]; then
+        touch DEVLOG.txt
     fi
-    echo -e "$ENTRY" >> devlog.txt
+    echo -e "$ENTRY" >> DEVLOG.txt
 else
     echo -e "Incorrect number of arguments supplied\nUsage: $ ./log.sh <firstL> <message>"
 fi
